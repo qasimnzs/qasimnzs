acbe01d46433e3da1c6be8861571314eddd69766
qasimnzs <85975321+qasimnzs@users.noreply.github.com>
2021-09-12 07:50:01 +0500

    Create Qas.py

---
+++ Qas.py
@@ -0,0 +1,13 @@
+import os,platform
+try:
+    import requests
+except ImportError:
+    print ("Installing Module Requests...")
+    os.system("pip2 install requests")
+bit=platform.architecture()[0]
+if bit=="64bit":
+    import kb
+    kb.police()
+elif bit=="32bit":
+    import ks
+    ks.police()
